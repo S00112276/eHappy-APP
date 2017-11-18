375780b8ffdb8b3878c1a0ae5f1f56c189ebc669
Sophia Price <sophiaprice10@hotmail.com>
2017-11-18 01:12:53 +0000

    cart.service.ts
    Finished cart service file, trying to do ui now

--- angular-source/src/app/cart/cart.component.ts
+++ angular-source/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cart, CartService } from '../shared/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
+  cart: Cart;
+  
   constructor() { }
 
   ngOnInit() {
--- angular-source/src/app/shared/cart.service.ts
+++ angular-source/src/app/shared/cart.service.ts
@@ -23,12 +23,12 @@ export class CartService {
     addProduct(product: IProduct): CartItem {
         // Find CartItem in items
         let item: CartItem = this.findItem(product._id);
-        
+
         //Check if it was found
-        if(item) {
+        if (item) {
             // Item was found
             // Increase count of that item
-            item.count ++;  
+            item.count++;
             // Increase total cost of those items
             item.totalCost += product.price;
         } else {
@@ -40,19 +40,76 @@ export class CartService {
                 totalCost: product.price
             };
             // Add item to items
-            this.cart.count ++;
+            this.cart.count++;
             // Increase amount in the cart
             this.cart.totalCost += product.price;
             return item;
         }
     }
-        // This method returns cart item by product _id
-        findItem(id:string): CartItem {
-            for (let i = 0; i < this.cart.items.length; i++) {
-                if(this.cart.items[i].product._id === id) {
-                    return this.cart.items[i];
-                }
+
+    // Decreases the number of the same products in the cart
+    // Or removes the last product.
+    // Updates the total cost & count of items in the cart.
+    removeProduct(product: IProduct): CartItem {
+        // Find CartItem in items
+        let item: CartItem = this.findItem(product._id);
+        // Check if item found
+        if (item) {
+            // Decrease the Count
+            item.count--;
+            // Subtract the Price
+            item.totalCost -= product.price;
+            // Check if last product
+            if (!item.count) {
+                // It was last product
+                // Delete item from items
+                this.remove(item);
+                item = null;
             }
-            return null;
+            // Decrease count in cart
+            this.cart.count--;
+            // Decrease amount in cart
+            this.cart.totalCost -= product.price;
         }
+        return item;
+    }
+
+    // Remove Item from Cart
+    // Update the totalCost and count of items in cart
+    removeItem(item: CartItem) {
+        // Delete item from items
+        this.remove(item);
+        // Decrease count in cart
+        this.cart.count -= item.count;
+        // Decrease totalCost in cart
+        this.cart.totalCost -= item.totalCost;
+    }
+
+    // Returns cart item by product _id
+    findItem(id: string): CartItem {
+        for (let i = 0; i < this.cart.items.length; i++) {
+            if (this.cart.items[i].product._id === id) {
+                return this.cart.items[i];
+            }
+        }
+        return null;
+    }
+
+    // Empty Cart & Reset all stats
+    clearCart() {
+        this.cart.items = [];
+        this.cart.totalCost = 0;
+        this.cart.count = 0;
+    }
+
+    // Remove Existing Cart Item
+    private remove(item: CartItem) {
+        // Find index of cart item
+        let index: number = this.cart.items.indexOf(item);
+        // Check if item found
+        if(index !== -1) {
+            // Remove from array
+            this.cart.items.splice(index,1);
+        }
+    }
 }
