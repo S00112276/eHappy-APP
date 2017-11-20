389fe29099eaaec9865784e20d83dbdc823e22f1
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-11-20 18:11:40 +0000

    need routing to be added

--- angular-source/src/app/app.component.ts
+++ angular-source/src/app/app.component.ts
@@ -1,64 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './shared/auth.service';
-import { Router } from '@angular/router';
-import { FlashMessagesService } from 'angular2-flash-messages';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
 
-  //for login
-  email: String;
-  password: String;
+  constructor() { }
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private flashMessage: FlashMessagesService
-
-  ) { }
   ngOnInit() {
   }
 
-  onLoginSubmit() {
-    //create object user for login
-    const user = {
-      email: this.email,
-      password: this.password
-    }
-
-    this.authService.authUser(user).subscribe(data => {
-        console.log("DBC"+data);
-      if (data.email.success) {
-        this.authService.storeUserData(data.token, data.user);
-        this.flashMessage.show('You are now logged in!', {
-          cssClass: 'alert-success',
-          timeout: 6000
-        });
-        this.router.navigate(['dashboard']);
-      }
-      else {
-        this.flashMessage.show(data.messages, {
-          cssClass: 'alert-danger',
-          timeout: 6000
-        });
-        this.router.navigate(['dashboard']);
-      }
-    });
-
-  }
-  // for navbar logout
-  onLogoutClick() {
-    this.authService.logout();
-    this.flashMessage.show('You are logged out!', {
-      cssClass: 'alert-succes',
-      timeout: 3000
-    });
-    this.router.navigate(['/dashboard']);
-    return false;
-  }
 }
--- angular-source/src/app/cart/cart.component.ts
+++ angular-source/src/app/cart/cart.component.ts
@@ -13,5 +13,4 @@ export class CartComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
