f4e33589b555fd4be996e34df2f16c51c3026c35
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-11-20 17:23:21 +0000

    validate email and register

--- angular-source/src/app/shared/validate.service.ts
+++ angular-source/src/app/shared/validate.service.ts
@@ -4,5 +4,20 @@ import { Injectable } from '@angular/core';
 export class ValidateService {
 
   constructor() { }
+  //validate Register 
+  validateRegister(user) {
+    if (user.name == undefined || user.email == undefined || user.username == undefined) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  //validate email
+  validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
 }
