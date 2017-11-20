34749e4eb5494483085908ee779d3c9004b7fb59
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-11-20 17:17:42 +0000

    package update and routes added

--- angular-source/src/app/app.module.ts
+++ angular-source/src/app/app.module.ts
@@ -1,26 +1,45 @@
-import { ProductService } from './shared/product.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+//services
+import { ProductService } from './shared/product.service';
+
+//components 
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+// adding routes
+const routes: Routes = [
+  { path: '', redirectTo: 'app', pathMatch: 'full' },
+  { path: 'app', component: AppComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent }
+  //   { path: 'product/:id', component: ProductDetailComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     CartComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FlashMessagesModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
