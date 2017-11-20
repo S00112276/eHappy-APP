f760de87168401fe47d35075bebcd6186fd88617
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-11-20 17:25:29 +0000

    auth service

--- angular-source/src/app/shared/auth.service.ts
+++ angular-source/src/app/shared/auth.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  authToken: any;
+  user: any;
+
+  constructor(
+    private http: Http
+  ) { }
+
+  //subscribe to observable
+  registerUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');// add value to the header
+    return this.http.post('http://localhost:3000/users/register', user, { headers: headers })
+      .map(res => res.json());
+  }
+
+  authUser(user) {
+    console.log("AS" + user.email);
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user.email, { headers: headers })
+      .map(res => res.json().data);
+  }
+
+  storeUserData(token, user) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 
 }
