e7d405c9a8eca287de9cfb6847e51a84e1a0b69a
Daniel O'Regan <S00112276@mail.itsligo.ie>
2017-11-27 12:59:11 +0000

    Refine style/ flash message
    Changed checkbox colour
    Changed flash message z-index and postion to follow UI practices

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // BodyParser Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 // Passport Middleware
 app.use(passport.initialize());
