ea71e9c4443876fc2c6ccc09e83884a717e33065
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-12-09 22:36:26 +0000

    /

--- angular-source/src/app/shared/product.service.ts
+++ angular-source/src/app/shared/product.service.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/do';
 
 @Injectable()
 export class ProductService {
-  private _productUrl = 'http://localhost:3055/products';
+  private _productUrl = '/products';
 
   constructor(private _http: HttpClient) { }
 
