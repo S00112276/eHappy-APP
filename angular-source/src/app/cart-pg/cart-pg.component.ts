5f006c82b67a61b9882df78ae53edced4ae3efbf
Sophia Price <sophiaprice10@hotmail.com>
2017-12-15 17:24:06 +0000

    Cart Styling

--- angular-source/src/app/cart-pg/cart-pg.component.ts
+++ angular-source/src/app/cart-pg/cart-pg.component.ts
@@ -58,6 +58,8 @@ export class CartPgComponent implements OnInit {
       currency: 'eur',
       amount: this.cart.totalCost * 100 // make it into euro instead of cents
     });
+
+    console.log(handler);
   }
 
 }
